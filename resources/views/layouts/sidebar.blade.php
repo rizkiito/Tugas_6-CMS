bb13d9267c57a62f1e22b5746c67b8bda019e6a5
rizkiito <125981746+rizkiito@users.noreply.github.com>
2023-04-05 22:24:35 +0700

    edit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,38 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+
+        </div>
+        <div class="sidebar-brand-text mx-3">shop garden </div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="{{route('dashboard')}}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <li class="nav-item active">
+        <a class="nav-link" href="{{route('barang')}}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Barang</span></a>
+    </li>
+
+
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+
+    <!-- Divider -->
+
+
+
+
+
+</ul>
